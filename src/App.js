e2572e383e6ae0b9212a9e4975423aecd052298a
Jay Kim <jeonghwan7191@gmail.com>
2023-10-03 20:08:13 -0400

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-/* eslint-disable */
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home"; // Import your Home component
+import About from "./pages/About"; // Import your About component
+import Profile from "./pages/Profile"; // Import your Home component
+import Writing from "./pages/Writing"; // Import your Writing component
 
 function App() {
-
-  let post = 'study';
-  let [글제목, b] = useState(['뿅뿅', '슉슉', '쁑쁑']);
-  
   return (
-    <>
-    <Navbar/>
-
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Define the Home route */}
+        <Route path="/about" element={<About />} /> {/* Define the About route */}
+        <Route path="/profile" element={<Profile />} /> {/* Define the Profile route */}
+        <Route path="/Writing" element={<Writing />} /> {/* Define the Writing route */}
+        {/* Define other routes for your application */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Home() {
+  return (
+      <h1>응애 우리 글쓸거야</h1>
+  );
+}
